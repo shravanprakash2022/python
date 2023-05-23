bd8db5b0b0704892e88532b67f7b11b4f0374c73
shravanprakash2022 <shravanprakash@gmail.com>
2023-05-24 01:48:33 +0530

    New Go lang Folder

--- python_lab/guessing_game.py
+++ python_lab/guessing_game.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import ranges
 import random
 import os
@@ -9,13 +10,26 @@ guess = None
 while guess != number:
     
     guess = int(input("Guess the number between 1 and 100: "))
+=======
+import random
+
+number = random.randint(1, 10)
+guess = None
+
+while guess != number:
+    guess = int(input("Guess the number between 1 and 10: "))
+>>>>>>> 85157dd (New Go lang Folder)
     if guess < number:
         print("Too low!")
     elif guess > number:
         print("Too high!")
     else:
+<<<<<<< HEAD
         print("Hurray !!!  You got it!")
         os.system('sleep 3')
         os.system('clear')
 
+=======
+        print("You got it!")
+>>>>>>> 85157dd (New Go lang Folder)
 
