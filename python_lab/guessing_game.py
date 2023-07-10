bff0b19f462ba286dfcc52cd54cb12410be82d8b
shravanprakash2022 <shravanprakash@gmail.com>
2023-07-10 16:36:32 +0530

    new new project

--- python_lab/guessing_game.py
+++ python_lab/guessing_game.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import ranges
 import random
 import os
 #from os import system
@@ -8,28 +6,11 @@ number = random.randint(1, 100)
 
 guess = None
 while guess != number:
-    
-    guess = int(input("Guess the number between 1 and 100: "))
-=======
-import random
-
-number = random.randint(1, 10)
-guess = None
-
-while guess != number:
     guess = int(input("Guess the number between 1 and 10: "))
->>>>>>> 85157dd (New Go lang Folder)
     if guess < number:
         print("Too low!")
     elif guess > number:
         print("Too high!")
     else:
-<<<<<<< HEAD
-        print("Hurray !!!  You got it!")
-        os.system('sleep 3')
-        os.system('clear')
-
-=======
         print("You got it!")
->>>>>>> 85157dd (New Go lang Folder)
 
